a93d64d42bf22f564c20c142d906c129b5487b61
oc34n <ocean.wrng@gmail.com>
2023-09-07 16:51:44 -0400

    Added controls for mesh based on user input

---
+++ src/components/character/controls/index.jsx
@@ -0,0 +1,52 @@
+//CUSTOM HOOK FOR USER INPUT 
+
+import { useEffect, useState } from "react";
+
+export const useInput = () => {
+    //Setting key value pairs with a boolean to determine position 
+    //character is moving
+    const [input, setInput] = useState({
+        forward: false,
+        backward: false,
+        left: false,
+        right: false,
+        shift: false
+    });
+
+    //Assign a key to an input 
+    const inputKeys = {
+        KeyW: 'forward',
+        KeyS: 'backward',
+        KeyA: 'left',
+        KeyD: 'right',
+        ShiftLeft: 'shift'
+    }
+
+    //Find the current key the user is pressing
+    //to updated boolean
+    const findInputKey = (key) => inputKeys[key];
+
+    useEffect(() => {
+
+        //Sets boolean values to either true or false
+        const handleKeyDown = (e) => {
+            setInput((m) => ({...m, [findInputKey(e.code)]: true}));
+        }
+        const handleKeyUp = (e) => {
+            setInput((m) => ({...m, [findInputKey(e.code)]: false}));
+        }
+
+        //Listens for user input
+        document.addEventListener('keydown', handleKeyDown)
+        document.addEventListener('keyup', handleKeyUp)
+
+        //Cleanup
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keyup', handleKeyUp)
+        }
+
+    },[])
+
+    return input;
+}
--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,28 +1,54 @@
-import React, { useRef, Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import Loader from "../loader";
+import React, { useRef, useEffect } from "react";
 import { useGLTF, PerspectiveCamera, useAnimations } from "@react-three/drei";
+import { useInput } from "./controls";
 
 export function Model(props) {
   const group = useRef();
+  const { forward, backward, left, right, shift } = useInput();
   const { nodes, materials, animations } = useGLTF("src/assets/blender-objects/character/testChar.gltf");
   const { actions } = useAnimations(animations, group);
-  
+
+  const currentAction = useRef('')
+
+  useEffect(() => {
+    let action = '';
+    console.log(action);
+    if(forward || backward || left || right) {
+      action = 'Walking';
+      if(shift) {
+        action = 'Running'
+      }
+    } else {
+      action = 'Idle'
+    }
+
+    if(currentAction.current != action) {
+      const nextAction = actions[action];
+      const current = actions[currentAction.current];
+      current?.fadeOut(0.2);
+      nextAction?.reset().fadeIn(0.2).play()
+      currentAction.current = action;
+    }
+
+  }, [forward, backward, left, right, shift]);
+
+
+
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
-        <group name="charactergltf">
+        <group name="charactergltf" position={[0.379, 0.573, 0.248]}>
           <group
             name="Camera"
-            position={[-2.095, 1.299, -6.528]}
-            rotation={[1.586, -0.005, 2.814]}
+            position={[0.044, 4.538, 8.801]}
+            rotation={[1.393, 0.001, -0.003]}
           >
             <PerspectiveCamera
               name="Camera_Orientation"
               makeDefault={false}
               far={1000}
               near={0.1}
-              fov={22.895}
+              fov={39.598}
               rotation={[-Math.PI / 2, 0, 0]}
             />
           </group>
@@ -91,7 +117,7 @@ export function Model(props) {
           </group>
         </group>
       </group>
-    </group>        
+    </group>   
   );
 }
 
