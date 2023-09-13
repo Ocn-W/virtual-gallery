5184e89458c466a0930bec8ccac31f9785a15ff6
oc34n <ocean.wrng@gmail.com>
2023-09-13 14:57:17 -0400

    Updated react router configurations

--- src/components/routes/root.jsx
+++ src/components/routes/root.jsx
@@ -1,10 +1,12 @@
 import Navigation from "../navigation";
+import App from "../../App";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
     return (
         <>
-        <Navigation/>
+        {/* <Navigation/> */}
+        <App/>
         <Outlet/>
         </>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { RouterProvider, createBrowserRouter, Outlet, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import Root from './components/routes/root';
-import Home from './components/home';
-import App from './App';
 import VirtualGallery from './components/virtual-gallery';
 import Checkout from './components/checkout';
+import Root from './components/routes/root';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>,
+    element: <Root/>,
   },
   {
   children: [
@@ -26,7 +24,5 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
